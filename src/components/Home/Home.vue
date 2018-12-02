f45f3496e2b7f83681b3849a168d4060ce20bf54
Irelia <1339521912@qq.com>
2018-12-02 11:27:42 +0800

    blog

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -7,7 +7,22 @@
           <div class="bg-box">
             <div class="bg-size">
               <div class="bg"></div>
-              <div class="lucency"></div>
+              <div class="lucency">
+                <p>12131</p>
+              </div>
+            </div>
+            <div class="bg-box1">
+              <div class="bg1"></div>
+              <div class="bg2"></div>
+            </div>
+            <div class="content">
+              <div class="item-title">
+                <a href="#">XXXXXX</a>
+              </div>
+              <div class="item-icon">
+                <div class="icon"></div>
+                <a href="#">sss</a>
+              </div>
             </div>
           </div>
         </div>
@@ -26,6 +41,12 @@ export default {
         { text: "专车" },
         { text: "顺风车" },
         { text: "出租车" },
+        { text: "代驾" },
+        { text: "巴士" },
+        { text: "快车" },
+        { text: "专车" },
+        { text: "顺风车" },
+        { text: "出租车" },
         { text: "代驾" }
       ]
     };
@@ -33,6 +54,8 @@ export default {
 };
 </script>
 <style lang='stylus' scoped>
+@import '~common/stylus/index.styl';
+
 .bgcolor {
   width: 100%;
   height: 100%;
@@ -43,7 +66,8 @@ export default {
   width: 900px;
   height: 1640px;
   margin: 0px auto;
-  padding-top: 30px;
+  position: relative;
+  top:100px;
   z-index: 200;
 
   .post-lists-body {
@@ -58,8 +82,9 @@ export default {
       .bg-box {
         width: 100%;
         height: 380px;
-        background: #fff;
         padding: 0;
+        position: relative;
+        overflow: hidden;
 
         .bg-size {
           width: 100%;
@@ -72,7 +97,7 @@ export default {
             display: block;
           }
 
-          &:hover:hover >.bg {
+          &:hover >.bg {
             filter: blur(1px);
             transform: scale(1.2);
           }
@@ -96,10 +121,91 @@ export default {
             background: #000;
             height: 250px;
             width: 100%;
+            cursor: pointer;
             position: absolute;
             display: none;
             -moz-opacity: 0.6;
             opacity: 0.6;
+          }
+        }
+
+        .bg-box1 {
+          width: 100%;
+          height: 80px;
+          padding: 0;
+          z-index: 999;
+          z-index: 9;
+          overflow: hidden;
+
+          // background: red;
+          .bg1 {
+            position: absolute;
+            z-index: 10;
+            width: 110%;
+            height: 100px;
+            background-color: #fff;
+            transform: rotate(10deg) translate(-16px, 0);
+          }
+
+          .bg2 {
+            position: absolute;
+            z-index: 9;
+            width: 110%;
+            height: 100px;
+            background: #000;
+            transform: rotate(-15deg) translate(10px, -10px);
+            opacity: 0.7;
+          }
+        }
+
+        .content {
+          width: 100%;
+          height: 130px;
+          background-color: #fff;
+          position: absolute;
+          bottom: 0;
+          z-index: 11;
+
+          .item-title {
+            width: 100%;
+            height: auto;
+
+            a {
+              font-size: 17px;
+              line-height: 17px;
+              word-break: break-all;
+              color: #313131;
+            }
+          }
+
+          .item-icon {
+            width: 100%;
+            height: auto;
+            padding: 0 15px 15px;
+            text-align: right;
+
+            a {
+              font-size: 16px;
+              position: relative;
+              float: right;
+              margin-right: 10px;
+              padding: 10px 0;
+              text-align: right;
+              text-transform: none;
+              color: #5f5f5f;
+            }
+
+            .icon {
+              display: inline-block;
+              float: right;
+              width: 42px;
+              height: 42px;
+              border: 1px solid #eaeaea;
+              border-radius: 50%;
+              background-image: url('./logo@3x.png');
+              background-position: 50% 50%;
+              background-size: cover;
+            }
           }
         }
       }
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -43,7 +43,10 @@ export default {
   width: 100%;
   height: 70px;
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1);
-  background-color : #fff ;
+  background-color: #fff;
+  position: fixed;
+  margin-bottom: 40px;
+  z-index: 9999;
 
   .center {
     position: relative;
