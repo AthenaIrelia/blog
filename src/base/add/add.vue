0511d8590ec31fa8c9cd01d8b7477ab235e1e2b9
Irelia <1339521912@qq.com>
2018-12-07 14:51:04 +0800

    blog

--- src/base/add/add.vue
+++ src/base/add/add.vue
@@ -1,12 +1,16 @@
 <!--  -->
 <template>
-  <div class="center">111233333331</div>
+  <div class="center">
+    <mavon-editor v-model="something" defaultOpen="preview" />
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      something:""
+    };
   },
 
   components: {},
@@ -20,14 +24,15 @@ export default {
 </script>
 <style lang='stylus' scoped>
 @import '~common/stylus/index.styl';
+@import '~./index.css';
+//我上次做音乐的 必须把哪个 放在这里 才拥有 不然还是没图标 放在 main.js 会在全局加载  但是我放在main。js中没用 
+.center {
+  width: 900px;
+  overflow: hidden;
+  background-color: #f7f7f7;
+  margin: 0px auto 110px;
+  position: relative;
 
-
-  .center {
-    width: 900px;
-    overflow: hidden;
-    background-color: #f7f7f7;
-    margin: 0px auto 110px;
-    position: relative;
-  }
-
+  
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'common/stylus/index.styl'
 import axios from 'axios'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import 'font-awesome/css/font-awesome.css'
+
+// use
+Vue.use(mavonEditor)
+
+
 Vue.config.productionTip = false
 import router from './Router'
 
